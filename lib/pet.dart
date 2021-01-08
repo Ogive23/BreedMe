9911ca74fac319b85b1cb0fa36516a670481a044
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-01-08 23:32:27 +0200

    Added Demo pets screen

---
+++ lib/pet.dart
@@ -0,0 +1,13 @@
+class Pet{
+  String id;
+  String ownerId;
+  String name;
+  int age;
+  String sex;
+  String type;
+  String notes;
+  String image;
+  bool status;
+  DateTime createdAt;
+  Pet(this.id,this.ownerId,this.name,this.age,this.sex,this.type,this.notes,this.image,this.status,this.createdAt);
+}
