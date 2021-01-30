4d2ac8d2bb7aea687487614c711c80f34ccd4e90
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-01-30 22:17:40 +0200

    Updated Dependencies

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:breed_me/main.dart';
+import 'file:///C:/Users/mahmo/AndroidStudioProjects/breed_me/lib/Screens/TopThree/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
